beef3e5c48620af9a942d447d9b9212ef761726c
Afiduddin, Riki <riki.afifuddin@ecomindo.com>
2022-07-19 11:15:41 +0700

    midleware authorization

--- pages/api/get/allusers.js
+++ pages/api/get/allusers.js
@@ -1,13 +1,16 @@
 import db from '../../../libs/db';
+import jwt from 'jsonwebtoken';
+import authorization from '../../../middlewares/authorization';
 
 export default async function handler(req,res) {
     if (req.method !== 'GET') return res.status(405).end();
     
-    const allusers = await db('posts');
+    const auth = await authorization(req, res);
+    const allurers = await db('users');  
     
     res.status(200);
     res.json({
         message: 'Success',
-        data: allusers
+        data: allurers
     })
 }
