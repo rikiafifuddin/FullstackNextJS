42b957d080f4fdd708943a871e3bb69a0cc9208f
Afiduddin, Riki <riki.afifuddin@ecomindo.com>
2022-07-19 11:25:37 +0700

    add middleware auth

--- pages/api/get/allusers.js
+++ pages/api/get/allusers.js
@@ -1,5 +1,4 @@
 import db from '../../../libs/db';
-import jwt from 'jsonwebtoken';
 import authorization from '../../../middlewares/authorization';
 
 export default async function handler(req,res) {
@@ -7,7 +6,7 @@ export default async function handler(req,res) {
     
     const auth = await authorization(req, res);
     const allurers = await db('users');  
-    
+
     res.status(200);
     res.json({
         message: 'Success',
--- pages/api/update/[id].js
+++ pages/api/update/[id].js
@@ -1,9 +1,11 @@
 import db from "../../../libs/db"
-
+import authorization from "../../../middlewares/authorization";
 export default async function handler(req, res){
 
     if (req.method !== 'PUT') return res.status(405).end();
     
+    const auth = await authorization(req, res);
+
     const {id} = req.query;
     const {title, content} = req.body;
 
